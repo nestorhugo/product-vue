4aa408c4a866c932823e65abfaca03e7a8c39f66
nestorhugo <nestorhugo@proton.me>
2024-06-20 17:08:10 -0300

    l3-end

--- main.js
+++ main.js
@@ -1,1 +1,10 @@
-const product = 'Socks'
+const app = Vue.createApp({
+  data: function () {
+    return {
+      product: "Socks",
+      description: "Very cool socks..",
+      image: "./assets/images/socks_green.jpg",
+      url: "https://github.com/",
+    };
+  },
+});
