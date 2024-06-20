a0d86ca08444c6e7d823ae0641cd02d234121b1d
nestorhugo <nestorhugo@proton.me>
2024-06-20 17:47:34 -0300

    list rendering

--- main.js
+++ main.js
@@ -5,6 +5,13 @@ const app = Vue.createApp({
       description: "Very cool socks..",
       image: "./assets/images/socks_green.jpg",
       url: "https://github.com/",
+      inStock: true,
+      details: ["50% cotton", "30% wool", "20% polyester"],
+      variants: [
+        { id: 1, color: "green" },
+        { id: 2, color: "blue" },
+      ],
+      sizes: ["Small", "Medium", "Large"],
     };
   },
 });
